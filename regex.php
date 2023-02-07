f971a6bbc2ef5f6439c5a63cc0ee0e09abf8847c
zidankhan86 <zidankhan718@gmail.com>
2023-02-07 20:34:32 +0600

    Printf and Vardump

--- regex.php
+++ regex.php
@@ -27,7 +27,7 @@ $exp=preg_match("/PHP|earn/i",$string,$array); //(preg_match) er maddome amra pa
 $exp=preg_match_all("/PHP|earn/i",$string,$array); // (preg_match_all) etao same but eta diye Shob data show korte pari
 //$exp=preg_match_all("/[^we]/i",$string,$array);
 //$exp=preg_match_all("/[aA-zZ]/",$string,$array);
-$exp=preg_match_all("/[1-9]/",$string,$array);
+//$exp=preg_match_all("/[1-9]/",$string,$array);
 
 if($exp){
 
